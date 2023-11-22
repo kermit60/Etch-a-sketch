55c7e9c04329916603af73522758440c32ced333
kermit <kermit.liu1212@gmail.com>
2023-11-22 13:48:17 -0500

    Update erase, change size, color button functions

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const grid = document.querySelector('.grid');
 const HEIGHT = 700;
+const grid = document.querySelector('.grid');
 const resetBut = document.querySelector('.reset');
+const changeColorBut = document.querySelector('.change-color');
+const erase = document.querySelector('.erase');
+const sizeChange = document.querySelector('.size');
+let color = 'black';
+color = document.querySelector('input').value;
+let isDown = false;
+
 
 // creating the grid of the etch-a-sketch
 let createDivs = numDiv => {
@@ -16,13 +23,28 @@ let createDivs = numDiv => {
     // updating the size of each cell based on the number of cells needed
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
-        cell.setAttribute('style', `height: ${HEIGHT/numDiv - 2}px; width: ${HEIGHT/numDiv - 2}px`);
-        cell.addEventListener('mouseover', () =>{
-            cell.style.backgroundColor = 'black';
+        cell.setAttribute('style', `height: ${HEIGHT/numDiv}px; width: ${HEIGHT/numDiv}px`);
+        
+        cell.addEventListener('mousedown', () => {
+            isDown = true;
         });
+
+        cell.addEventListener('mousemove', () => {
+            if (isDown) {
+                cell.style.backgroundColor = color;
+            }
+        });
+
+        cell.addEventListener('mouseup', () => {
+            if (isDown) {
+                isDown = false;
+            }
+        });
+        
     })
 }
 
+// resetting the grid 
 resetBut.addEventListener('click', () => {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
@@ -30,5 +52,27 @@ resetBut.addEventListener('click', () => {
         
     })
 });
+// changing the color of the paint
+changeColorBut.addEventListener('click', () => {
+    color = document.querySelector('input').value;
+});
 
-createDivs(15);
+// making the erase function
+erase.addEventListener('click', ()=> {
+    color = 'white';
+});
+
+sizeChange.addEventListener('click', () => {
+    let size = parseInt(prompt("Please enter grid-size: 16-100"));
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    createDivs(size)
+});
+
+// needs to do:
+// change color of the colouring paint completed
+// hover and mousedown painting
+
+
+createDivs(16);
